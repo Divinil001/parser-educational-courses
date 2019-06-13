7bf62f43fb1e36423b56a157ceebea3f0574bcb4
okeyrita <koteika0909@gmail.com>
2019-06-14 01:31:11 +0300

    Change spider catalonia

--- exchange_program/exchange_program/spiders/peregudova_spider.py
+++ exchange_program/exchange_program/spiders/peregudova_spider.py
@@ -1,67 +1,60 @@
 import scrapy
 from urllib.parse import urljoin
 from scrapy.utils.markup import remove_tags
-
-CATALONIA_URL = 'https://www.fib.upc.edu'
+import re
 
 class CataloniaSpider(scrapy.Spider):
     name = "catalonia"
     start_urls = [
-        urljoin(CATALONIA_URL, '/en/mobility/incoming/how-apply'),
+        'https://www.fib.upc.edu/en/studies/bachelors-degrees/bachelor-degree-informatics-engineering/curriculum/syllabus',
+        'https://www.fib.upc.edu/en/studies/masters/master-informatics-engineering/curriculum/syllabus',
+        'https://www.fib.upc.edu/en/studies/masters/master-innovation-and-research-informatics/curriculum/syllabus',
+        'https://www.fib.upc.edu/en/studies/masters/master-artificial-intelligence/curriculum/syllabus',
     ]
 
-    def get_courses(self, ul , next , links, semester):
+    def parse_credits(self , response):
+        credits = response.css('.col-xs-9.col-md-9 ::text').get()
 
-        # разделители для удаления невалидной информации из строк
-        d1 = "'s degree in"
-        d2 = '-'
-        d3 = ' '
-        d4 = 'Exchange period ('
+    def parse(self, response):
+        present_link = response.request.url
+        course_level = response.css('.breadcrumb') # for  4. Course_level - string
+        course_level = course_level.css('a')[3]
+        course_level = re.sub(r" Degree in ",", ",remove_tags(course_level.extract()))
+        course_level = re.sub(r" in ",", ",remove_tags(course_level))
+        table = response.css('.field-item.even') # for parse 2,3,4,5,8
 
-        semester =  remove_tags(semester.extract()).replace(d4, '').strip(')')
-        #e = semester.sub(d4,'',semester)
         courses = []
+        university_title = "Universitat Politechnica de Catalunya Barcelonatech.FIB Facultat d'Informatica de Barcelona."
+        credits_type = "ECTS" # because in throughout Spain credits type is ECTS
 
-        i = 0
-        for x , y , z  in zip( ul.css('li'), next.css('li'), links):
-            degree, rest = remove_tags(x.extract()).split(d1)
-            name, id = rest.split(d2)
+        lines = table.css('tr')
+        for line in lines:
+            id = line.css('.acronym ::text').get()  # 2. Course_local_id - string
+            if id == "Acronym": #  иииииии так сойдет
+                continue
+            name = line.css('.name ::text').get() # 3. Course_title_full - string
+            link = urljoin(present_link , line.css('.name a::attr(href)').get()) #8. Course_link_descrip - string
+            global credits
+            if link is not None:
+                yield response.follow(link, self.parse_credits)
+#            print(credits)
+            semester = line.css('.curs_obert ::text').get() # 5. Course_semester - string
+            if (semester == "Q1, Q2"):
+                semester = "Spring, Fall"
+            elif (semester == "Q2"):
+                semester = "Spring"
+            elif (semester == "Q1"):
+                semester = "Fall"
 
-            buffer, credits = remove_tags(y.extract()).split(d2)
-            credits = credits.strip()
-            credits_num, credits_type = credits.split(d3)
-
-            #1. University_title_full - string
-            university_title = "Universitat Politechnica de Catalunya Barcelonatech.FIB Facultat d'Informatica de Barcelona."
-
-            courses.append({
-                'university_title' : university_title, # 1. University_title_full - string
-                'id': id.strip(), # 2. Course_local_id - string
-                'name': name.strip(), # 3. Course_title_full - string
-                'degree': degree.strip(), # 4. Course_level - string
-                'semester': semester, # 5. Course_semester - string
-                'credits_num' : credits_num.strip(), # 6.Course_credits - integer
-                'credits_type' : credits_type.strip(),# 7. Course_credits_type
-                'links' : urljoin(CATALONIA_URL, links[i]), #8. Course_link_descrip - string
-            })
-            i+=1
-
-        return courses
-
-    def parse(self, response): # 4 пункт парсинга
-        alerts = response.css('.alert')
-        ul = alerts[3].css('ul ul')[0] # для парсинга 2,3,4 пунктов
-        next = alerts[3].css('ul ul')[2] # для парсинга 6, 7 пунктов
-
-        links = next.css('a::attr(href)').extract() #
-        semester = alerts[1].css('ul li')[2] # для 8 пункта
-
-        courses = self.get_courses(ul , next , links, semester)
-        print(courses)
-        
-        #print(links)
-        #print([remove_tags(x) for x in semester.getall()])
-        #print(urls.getall()
-
-class Spider(scrapy.Spider):
-    name = "catalonia"
+        courses.append({
+            'university_title' : university_title, # 1. University_title_full - string
+            'id': id.strip(), # 2. Course_local_id - string
+            'name': name.strip(), # 3. Course_title_full - string
+            'course_level': course_level.strip(), # 4. Course_level - string
+            'semester': semester, # 5. Course_semester - string
+            'credits_num' : credits.strip(), # 6.Course_credits - integer
+            'credits_type' : credits_type,# 7. Course_credits_type
+            'link' : link, #8. Course_link_descrip - string
+        })
+#    credits = None
+        #print(courses)
