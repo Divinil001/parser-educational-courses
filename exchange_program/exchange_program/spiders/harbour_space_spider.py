dfa95cfd16c4b22816ed6d5cc1db3d10923625b0
mintyque <35532094+mintyque@users.noreply.github.com>
2019-06-21 07:20:23 +0300

    Update harbour_space_spider.py

--- exchange_program/exchange_program/spiders/harbour_space_spider.py
+++ exchange_program/exchange_program/spiders/harbour_space_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from exchange_program.exchange_program.items import ExchangeProgramItem
+from urllib.parse import urljoin
 #ref https://raw.githubusercontent.com/mGalarnyk/Python_Tutorials/master/Scrapy/fundrazr/fundrazr/spiders/fundrazr_scrape.py
 
 UNIVERSITY_TITLE = "Harbour.Space University"
@@ -13,25 +14,24 @@ class HarbourSpaceSpider(scrapy.Spider):
 
     def parse(self, response):
         for href in response.css("//li[contains(@class, 'subjects__item')]"):
-            # TODO Better to use urljoin() method from Scrapy
-            url = "https:" + href.extract()
+            url = urljoin("https:", href.extract())
             yield scrapy.Request(url, callback=self.parse_course)
 
     def parse_course(self, response):
         item = ExchangeProgramItem()
-        item['university_title'] = UNIVERSITY_TITLE
+        item['university_title_full'] = UNIVERSITY_TITLE
 
         # TODO Better to use get() method instead of extract() since it is safer
-        item['course_title'] = response.xpath("//div[contains(@class, 'subject__item__title')]"
-                                              "/descendant::text()").extract()[0].strip()
+        item['course_title_full'] = response.xpath("//div[contains(@class, 'subject__item__title')]"
+                                              "/descendant::text()").get()
 
-        item['semester'] = 'year X'
+        item['course_semester'] = 'year X'
 
         item['course_credits'] = response.xpath("//div[contains(@class, 'subject__item__credits')]"
                                                 "/span[contains(@class, 'subjects__item__credits__count')]"
-                                                "/descendant::text()").extract()[0].strip()
+                                                "/descendant::text()").get()
 
-        item['credits_type'] = CREDIT_TYPE
+        item['course_credits_type'] = CREDIT_TYPE
         yield
 
 
