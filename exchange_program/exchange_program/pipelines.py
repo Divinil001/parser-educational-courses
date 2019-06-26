83745024a791c6dbcabe174be26c3d7ab918314d
okeyrita <koteika0909@gmail.com>
2019-06-26 16:54:58 +0300

    .

--- exchange_program/exchange_program/pipelines.py
+++ exchange_program/exchange_program/pipelines.py
@@ -6,16 +6,30 @@ class CataloniaPipeline(object):
         pass
 
     def close_spider(self, spider):
+        finished_courses = []
         for course in spider.courses:
             try:
-
                 #combine results to one set
                 course['course_credits'] = spider.course_credits[course['course_link_descrip']]
+
+                finished_courses.append({
+                    'university_title_full': course['university_title_full'],  # 1. University_title_full - string
+                    'course_local_id': course['course_local_id'],  # 2. Course_local_id - string
+                    'course_title_full': course['course_title_full'],  # 3. Course_title_full - string
+                    'course_level': course['course_level'],  # 4. Course_level - string
+                    'course_semester': course['course_semester'],  # 5. Course_semester - string
+                    'course_credits': spider.course_credits[course['course_link_descrip']],
+                    'course_credits_type': course['course_credits_type'],  # 7. Course_credits_type
+                    'course_link_descrip': course['course_link_descrip'],  # 8. Course_link_descrip - string
+                    'course_track': course['course_track'],
+                })
             except:
                 pass
+        # создадим словарь с нужным порядком ключей
+
 
         with open('dataset.json', 'w') as file: # save data to file
-            json.dump(spider.courses, file)
+            json.dump(finished_courses, file)
 
     def process_item(self, item, spider):
         return item
