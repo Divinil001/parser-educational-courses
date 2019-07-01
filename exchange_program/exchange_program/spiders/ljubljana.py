d8d38364fbaa954bb65f751f64ee7651a19bfb0c
Egor <egor@Egors-MacBook-Pro.local>
2019-07-01 22:05:32 +0300

    Reply to code review`

--- exchange_program/exchange_program/spiders/ljubljana.py
+++ exchange_program/exchange_program/spiders/ljubljana.py
@@ -13,24 +13,24 @@ class LjubljanaSpider(scrapy.Spider):
             data[x] = data[x].replace("\xa0", " ")
         print(len(data))
         index = 0
-        # TODO Better to use None in the situations like this
-        course_level = ''  # TODO Change to None
+       
+        course_level = None
 
-        # TODO It would be better to increment your index by 6 instead of multiplying each time on 6
-        #  It would increase code readability
-        while index*6 < len(data):
-            if data[index*6] == ' Code' or data[index*6] == 'Code':
-                if data[(index*6)+1] != "Course":
-                    course_level = data[(index*6)+1]
+        
+        while index < len(data):
+            if data[index] == ' Code' or data[index] == 'Code':
+                if data[(index)+1] != "Course":
+                    course_level = data[(index)+1]
                 index += 1
             course = dict()
             course['University_title_full'] = 'University of Ljubljana'
-            course['Course_local_id'] = data[index*6]
-            course['Course_title_full'] = data[(index*6)+1]
+            course['Course_local_id'] = data[index]
+            course['Course_title_full'] = data[(index)+1]
             course['Course_level'] = course_level
-            course['Course_semester'] = data[(index*6)+2]
-            course['Course_credits'] = data[(index*6)+3]
+            course['Course_semester'] = data[(index)+2]
+            course['Course_credits'] = data[(index)+3]
             course['Course_credits_type'] = 'ECTS'
             # TODO Add link to the course itself
-            index += 1
+            # Response: there's no link
+            index += 6
             yield course
